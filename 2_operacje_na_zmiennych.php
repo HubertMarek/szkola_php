0b76db5d477e1c9bf539e67a962dc3cd345e3538
Hubert Marek <marekhubert.80@gmail.com>
2020-09-24 13:44:46 +0200

    dodano rzutowanie zmiennych

--- 2_operacje_na_zmiennych.php
+++ 2_operacje_na_zmiennych.php
@@ -42,4 +42,35 @@ echo $y; // 6
 $y = ++$x * 2 - 1;
 echo $x; // 8
 echo $y; // 15
- ?>
+// operatory rzutowania
+// bool, int, float, string, array, object, unset
+
+$test1 = '123abc45';
+
+echo '<hr>Typ danych $test1: ', gettype($test1), '<br>';
+echo "$test1<br>";
+$test1 = (int)$test1;
+echo "$test1<br>";
+echo 'Typ danych $test1: ', gettype($test1), '<br>';
+
+
+$test2 = 0;
+
+echo '<hr>Typ danych $test2: ', gettype($test2), '<br>';
+echo "$test2<br>";
+$test2 = (bool)$test2;
+echo "$test2<br>";
+echo 'Typ danych $test2: ', gettype($test2), '<br>';
+
+
+$test3 = 0;
+
+echo '<hr>Typ danych $test3: ', gettype($test3), '<br>';
+echo "$test3<br>";
+$test3 = (unset)$test3;
+echo "$test3<br>";
+echo 'Typ danych $test3: ', gettype($test3), '<br>';
+
+
+
+?>
