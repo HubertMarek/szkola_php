b24a015511303980905c33585b72918d03ea9e2d
user <user@V11D.edu.st.poznan.pl>
2020-10-08 14:47:35 +0200

    finalna wersja formularza

--- 5_1_form.php
+++ 5_1_form.php
@@ -5,23 +5,27 @@
 </head>
 
 <body>
-  <form method="get">
-    <input type="text" name="surname" autofocus><br><br>
-    <input type="submit" value="wyślij"><hr>
-  </form>
-
-  <?php
+    <?php
   if (!empty($_GET['surname']))
   {
     $surname = $_GET['surname'];
     if(!empty($surname))
     {
-      echo "Nazwisko: $surname";
+      echo "Nazwisko: $surname<hr>";
+      echo "<a href=\"./5_1_form.php\">Powrót do formularza</a>";
     }
   }
   else
   {
+    $formularz = <<<FORM
+    <form method="get">
+      <input type="text" name="surname" autofocus><br><br>
+      <input type="submit" value="wyślij"><hr>
+    </form>
+FORM;
+    echo $formularz;
     echo 'Wyślij formularz';
+
   }
    ?>
 
